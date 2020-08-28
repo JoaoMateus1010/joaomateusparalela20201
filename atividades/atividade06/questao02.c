209cec8fa26c39b4b0c3649d6b2719a576fb5ada
JoaoMateus1010 <joaomateus102030@gmail.com>
2020-08-28 01:42:20 -0300

    Atividade 06

---
+++ atividades/atividade06/questao02.c
@@ -0,0 +1,52 @@
+// Cálculo das Médias
+#include <stdio.h>
+#include <stdlib.h>
+#include <mpi.h>
+                
+int main(int argc, char *argv[]) {
+    int i, rank, size;
+    int *input_array, *output_array;
+
+    MPI_Init(&argc, &argv);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+    input_array = (int *) malloc(size * sizeof(int));
+    output_array = (int *) malloc(size * sizeof(int));
+
+    for (i = 0; i < size; i++)
+        input_array[i] = rank * size + i;
+
+    printf("Input for process %d\n", rank);
+    for (i = 0; i < size; i++)
+        printf("%d ", input_array[i]);
+    printf("\n");
+
+    //MPI_Alltoall(input_array, 1, MPI_INT, output_array, 1, MPI_INT, MPI_COMM_WORLD);
+
+    /*
+    
+    MPI_Gather(inbuffer,1,MPI_INT,outbuffer,1,MPI_INT,ROOT,MPI_COMM_WORLD);
+
+    */
+   MPI_Status *st;
+    for(int i=0;i<size;i++){        
+        //Tentei fazer com o Gather mas não encontrei a lógica, não sei se era possível, e se é, não sei como se faz.
+        // Também não sei se era usadn send e recv que foi pedido, mas só consegui fazer assim.
+
+        //MPI_Gather(&input_array[rank],1,MPI_INT,&output_array[i],1,MPI_INT,0,MPI_COMM_WORLD);   
+                             
+        MPI_Send(&input_array[rank],1,MPI_INT,i,0,MPI_COMM_WORLD);
+        MPI_Recv(&output_array[i],1,MPI_INT,i,0,MPI_COMM_WORLD,st);
+    }
+
+    printf("Output for process %d\n", rank);
+    for (i = 0; i < size; i++)
+        printf("%d ", output_array[i]);
+    printf("\n");
+
+    free(input_array);
+    free(output_array);
+    MPI_Finalize();
+    return 0;
+}
---
+++ atividades/atividade06/questao03.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <mpi.h>
+
+#define ROOT    (0)
+
+int main(int argc,char* argv[]){   
+    double t1,t2;         
+    int numtasks,rank,len,rc;
+    char hostname[MPI_MAX_PROCESSOR_NAME];
+    float number_vect_0,number_vect_1;
+    int size_both_vect,chunk;    
+    float total_sum;
+    sscanf (argv[1], "%f", &number_vect_0);     
+    sscanf (argv[2], "%f", &number_vect_1);     
+    sscanf (argv[3], "%d", &size_both_vect);     
+    
+    float* vect_0=(float*)calloc(size_both_vect,sizeof(float));
+    float* vect_1=(float*)calloc(size_both_vect,sizeof(float));
+
+    for(int i=0;i<size_both_vect;i++){
+        vect_0[i]=number_vect_0;
+        vect_1[i]=number_vect_1;
+    }               
+    
+    MPI_Init(&argc,&argv); // Inicializando o MPI    
+    MPI_Comm_size(MPI_COMM_WORLD,&numtasks);//Recupera o número de tasks
+    MPI_Comm_rank(MPI_COMM_WORLD,&rank);//recupera o identificador de cada processo
+    MPI_Get_processor_name(hostname,&len);//Recupera o nome da máquina    
+    if(rank==ROOT){
+        t1 = MPI_Wtime();
+    } 
+    chunk = (int)size_both_vect/numtasks;        
+
+    float* vet_temp_0 = (float*)calloc(chunk,sizeof(float));        
+    float* vet_temp_1 = (float*)calloc(chunk,sizeof(float));      
+
+    MPI_Scatter(vect_0,chunk,MPI_FLOAT,vet_temp_0,chunk,MPI_FLOAT,ROOT,MPI_COMM_WORLD);    
+    MPI_Scatter(vect_1,chunk,MPI_FLOAT,vet_temp_1,chunk,MPI_FLOAT,ROOT,MPI_COMM_WORLD);            
+
+    for(int i=0;i<chunk;i++){
+        vet_temp_0[i]*=vet_temp_1[i];
+    }
+    
+    MPI_Gather(vet_temp_0,chunk,MPI_FLOAT,vect_0,chunk,MPI_FLOAT,ROOT,MPI_COMM_WORLD);
+    if(rank==ROOT){
+        for(int i=0;i<size_both_vect;i++){
+            total_sum+=vect_0[i];
+        }
+        //printf("PRODUTO: %.1f",total_sum);
+    }
+    if(rank==ROOT){ t2 = MPI_Wtime();}
+    if(rank==ROOT) printf("%f",t2-t1);                    
+    MPI_Finalize();//Finaliza o MPI
+    free(vect_0);
+    free(vect_1);        
+    return 0;
+}
